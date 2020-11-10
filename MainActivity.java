19a802645bd5367b616a3a8e637e865ed98830c7
MikeThimios <73944121+MikeThimios@users.noreply.github.com>
2020-11-10 15:34:59 +0200

    Add files via upload
    commitSudoken

---
+++ MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void btnZero (View view){
+
+    }
+    public void btnOne (View view){
+
+    }
+    public void btnTwo (View view){
+
+    }
+    public void btnThree (View view){
+
+    }
+    public void btnFour (View view){
+
+    }
+    public void btnFive (View view){
+
+    }
+    public void btnSix (View view){
+
+    }
+    public void btnSeven (View view){
+
+    }
+    public void btnEight (View view){
+
+    }
+}
