e953a2795bb1770c1fb36855f8c0c93fa24cfc5a
terebravisse <gnarishtnt@gmail.com>
2020-11-11 08:16:35 +0200

    A grim start. No going back.

--- MainActivity.java
+++ MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void btnZero (View view){
+    /*public void btnZero (View view){
 
     }
     public void btnOne (View view){
@@ -54,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void btnEquals (View view){
 
-    }
+    }*/
 
 
     // slasher
